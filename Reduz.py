203a265626d8b113b3a1b5016f66fef402170a53
mcszen1 <mcszen@gmail.com>
2024-02-14 10:19:50 -0300

    Update Reduz.py

--- Reduz.py
+++ Reduz.py
@@ -44,7 +44,7 @@ def create_download_button(i, img_byte_arr):
     )
 
 def main():
-    st.image('labcom_logo_preto.jpg')
+    st.image('NIDLogo.jpg')
     st.title("DOWNSCALER")
     st.write('REDUTOR DE IMAGENS')
     st.write('Usar imagens mais leves no seu site facilita o carregamento e a experiência do usuário na sua página. Assim, se você tem imagens de tamanho maior que não precisam ser tão pesadas, basta usar o image-web-resizer para converter várias delas ao mesmo tempo ( redução em lote ) e depois apenas fazer o download das imagens . Você faz o upload das imagens que quiser (limitadas a 200 mega no total). Formatos aceitos: jpg, jpeg, png.')
